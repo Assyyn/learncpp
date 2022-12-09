b1ac73674cd2cc7697ec4295d2fd077f414930d9
yuukii <hzatama167@gmail.com>
2022-12-09 20:12:34 +0545

    started chapter 10

---
+++ 10-compound-types-B/10.01-user-defined-types.cpp
@@ -0,0 +1,15 @@
+// an example of a user(program)-defined Fraction type:
+// This only defines what a Fraction type looks like, it doesn't create one
+struct Fraction {
+  int numerator{};
+  int denominator{};
+};
+
+int main() {
+  Fraction f{3, 4}; // this actually instantiates a Fraction object named f
+
+  return 0;
+}
+
+// By convention, program-defined types are named starting with a capital letter
+// and don't use a suffix
